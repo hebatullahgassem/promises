4af27309ca9aaa4fa1a542da8c465d432911a78b
hebatullahgassem <hebatullahgassem@gmail.com>
2024-01-31 22:47:29 +0200

    sec commit

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 const container = document.querySelector('.container');
 
-const request = new XMLHttpRequest();
+async function fetchProducts(){
+  
+  try { 
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
+  const {products} = await data;
 
-request.open("GET", "https://dummyjson.com/products");
-request.send();
-
-request.addEventListener("load", function(){
-  if(request.status === 404) handleErr();
-  const {products} = JSON.parse(this.responseText);
-    products.forEach(product => {
+    products.map((product) => {
       //card
       const cardContainer = document.createElement('div');
       cardContainer.className = 'card';
       container.appendChild(cardContainer);
-
+  
       //product title
       const title = document.createElement('h4');
       title.textContent = product.title;
       cardContainer.appendChild(title);
-
+  
       //product descripsion
       const description = document.createElement('p');
       description.textContent = product.description;
       cardContainer.appendChild(description);
-
+  
       //product image
       const image = document.createElement('img');
       image.src = product.images[0];
       image.className = 'img';
       cardContainer.appendChild(image);
-
+  
       //product price
       const price = document.createElement('h5');
       price.textContent = `${product.price} EGP`;
       cardContainer.appendChild(price);
     });
-});
 
-request.addEventListener("error", handleErr);
-
-function handleErr(){
-    //err message
-    const err = document.createElement('span');
-    err.textContent = 'Not Found!'
-    err.className = 'err';
-    container.appendChild(err);
+  } catch(err) {
+     // err message
+      err = document.createElement('span');
+      err.textContent = 'Not Found!'
+      err.className = 'err';
+      container.appendChild(err);
+  }
 }
+
+fetchProducts();
